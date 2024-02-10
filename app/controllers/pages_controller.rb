09ee5cfd026d0d52d5466f51a64502e3a22ece4c
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2024-02-10 07:56:08 -0500

    Fix avatar if current user has no image

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :restrict_access
+
   def root
+  end
+
+  private
+
+  def restrict_access
+    redirect_to root_path unless current_user
   end
 end
